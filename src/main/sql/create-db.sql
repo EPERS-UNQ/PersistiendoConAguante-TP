984cae3a5d13a719af68daabe618510daf8f4d1c
Astaroth <psljp@protonmail.com>
2018-04-08 23:46:32 -0300

    sql para crear y borrar test db

---
+++ src/main/sql/create-db.sql
@@ -0,0 +1,17 @@
+DROP SCHEMA IF EXISTS epers_woe;
+CREATE SCHEMA epers_woe;
+
+USE epers_woe;
+
+CREATE TABLE raza (
+  id int NOT NULL AUTO_INCREMENT,
+  idRaza int not null UNIQUE,
+  nombre VARCHAR(255) NOT NULL UNIQUE,
+  clases VARCHAR(255) NOT NULL,
+  peso int NOT NULL,
+  alt int NOT NULL,
+  energiaI int NOT NULL,
+  urlFoto VARCHAR(255) NOT NULL,
+  cantP int NOT NULL,
+  PRIMARY KEY (id)
+);
