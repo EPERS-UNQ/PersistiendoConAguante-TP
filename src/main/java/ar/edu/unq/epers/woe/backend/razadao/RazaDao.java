7c8a3d609d166b95b4e0f92599c9c5c6ba594034
Astaroth <psljp@protonmail.com>
2018-04-08 23:45:39 -0300

    cambios para test guardar

--- src/main/java/ar/edu/unq/epers/woe/backend/razadao/RazaDao.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/razadao/RazaDao.java
@@ -8,20 +8,29 @@ import java.sql.SQLException;
 import ar.edu.unq.epers.woe.backend.model.raza.Raza;
 
 public class RazaDao {
+
+	//registra el driver que se va a usar al instanciar la clase
+	public RazaDao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("No se puede encontrar la clase del driver", e);
+		}
+	}
 	
 	//inserta los datos de una raza en la db
 	public void guardar(Raza raza) {
 		this.executeWithConnection(conn -> {
 			PreparedStatement ps = conn.prepareStatement(
 					"INSERT INTO raza (idRaza, nombre, clases, peso, alt, energiaI, urlFoto, cantP ) VALUES (?,?,?,?,?,?,?,?)");
-			ps.setString(1, raza.getId().toString());
-			ps.setString(1, raza.getNombre());
-			ps.setString(1, raza.getClases().toString());
-			ps.setString(1, new Integer(raza.getPeso()).toString() );
-			ps.setString(1, new Integer(raza.getAltura()).toString() );
-			ps.setString(1, new Integer(raza.getEnergiaInicial()).toString() );
-			ps.setString(1, raza.getUrlFoto());
-			ps.setString(1, new Integer(raza.getCantidadPersonajes()).toString() );
+			ps.setInt(1, raza.getId());
+			ps.setString(2, raza.getNombre());
+			ps.setString(3, raza.getClases().toString());
+			ps.setInt(4, new Integer(raza.getPeso()));
+			ps.setInt(5, new Integer(raza.getAltura()));
+			ps.setInt(6, new Integer(raza.getEnergiaInicial()));
+			ps.setString(7, raza.getUrlFoto());
+			ps.setInt(8, new Integer(raza.getCantidadPersonajes()));
 
 			ps.execute();
 
@@ -34,7 +43,6 @@ public class RazaDao {
 		}
 		);
 	}
-	
 	
 	/**
 	 * Ejecuta un bloque de codigo contra una conexion.
