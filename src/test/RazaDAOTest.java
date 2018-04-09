dcf10bf2fccd38cf4e0b5f833fbc09e9a7ca0ba1
Astaroth <psljp@protonmail.com>
2018-04-09 20:37:22 -0300

    completado primer test de guardar raza; agregado método tearDown

--- src/test/RazaDAOTest.java
+++ src/test/RazaDAOTest.java
@@ -1,13 +1,12 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import ar.edu.unq.epers.woe.backend.model.raza.Clase;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Set;
 import ar.edu.unq.epers.woe.backend.model.raza.Raza;
 import ar.edu.unq.epers.woe.backend.razadao.RazaDao;
-
 
 
 public class RazaDAOTest {
@@ -22,19 +21,23 @@ public class RazaDAOTest {
         clases.add(Clase.SACERDOTE);
         clases.add(Clase.MAGO);
 
-        this.raza.setNombre("raza1");
+        this.raza.setNombre("raza2");
         this.raza.setAltura(55);
         this.raza.setClases(clases);
         this.raza.setEnergiaIncial(10);
-        this.raza.setId(1);
         this.raza.setPeso(50);
         this.raza.setUrlFoto("url_dest");
         this.raza.setCantidadPersonajes(0);
     }
 
     @Test
-    public void al_guardar_y_luego_recuperar_raza_se_obtiene_objetos_similares() {
-        this.razaDAO.guardar(this.raza);
+    public void al_guardar_primera_raza_se_obtiene_primerId_disponible() {
+        this.razaDAO.crearRaza(this.raza);
+        assertEquals(this.razaDAO.nextId(), new Integer(2));
     }
 
+    @After
+    public void tearDown() {
+        this.razaDAO.deleteAll();
+    }
 }
