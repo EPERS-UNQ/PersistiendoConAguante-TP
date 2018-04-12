7bf149cf0c95e361f50766884eb2477b7bad8d4c
Astaroth <psljp@protonmail.com>
2018-04-12 19:43:41 -0300

    agregado test de crearPersonaje

--- src/test/RazaDAOTest.java
+++ src/test/RazaDAOTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import ar.edu.unq.epers.woe.backend.model.raza.Clase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +19,7 @@ public class RazaDAOTest {
     @Before
     public void crearModelo() {
         this.razaDAO.crearSetDatosIniciales();
-        this.raza = this.razaDAO.getRaza(1);
+        this.raza = new Raza().getRaza(1);
     }
 
     @Test
@@ -27,7 +29,7 @@ public class RazaDAOTest {
 
     @Test
     public void al_recuperar_una_raza_se_crea_una_instancia_con_atributos_correctos() {
-        Raza raza = this.razaDAO.getRaza(1);
+        Raza raza = this.raza.getRaza(1);
         assertEquals(raza.getId(), new Integer(1));
         assertEquals(raza.getNombre(), this.raza.getNombre());
         assertEquals(raza.getClases(), this.raza.getClases());
@@ -39,9 +41,16 @@ public class RazaDAOTest {
 
     @Test
     public void al_recuperar_la_lista_de_razas_estan_ordenadas_alfabeticamente() {
-        List<Raza> razas = this.razaDAO.getAllRaza();
+        List<Raza> razas = this.raza.getAllRazas();
         assertEquals(razas.get(0).getNombre(), "xRaza1");
         assertEquals(razas.get(1).getNombre(), "yRaza2");
+    }
+
+    @Test
+    public void al_crear_pj_se_incrementan_pjs_de_la_raza() {
+        Integer cantPrevia = this.raza.getCantidadPersonajes();
+        this.raza.crearPersonaje(this.raza.getId(), "Seiya", Clase.CABALLERO);
+        assertEquals(this.raza.getRaza(this.raza.getId()).getCantidadPersonajes(), cantPrevia + 1);
     }
 
     @After
