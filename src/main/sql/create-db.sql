b77b543cd4695dc8dd4174cc58e813543c518d93
navani <navani@navani>
2018-04-13 18:00:46 -0300

    cambio en auto increment id

--- src/main/sql/create-db.sql
+++ src/main/sql/create-db.sql
@@ -1,11 +1,11 @@
-DROP SCHEMA IF EXISTS epers_woe;
+﻿DROP SCHEMA IF EXISTS epers_woe;
 CREATE SCHEMA epers_woe;
 
 USE epers_woe;
 
 CREATE TABLE raza (
   id int NOT NULL AUTO_INCREMENT,
-  idRaza int not null UNIQUE AUTO_INCREMENT,
+  idRaza int not null UNIQUE,
   nombre VARCHAR(255) NOT NULL UNIQUE,
   clases VARCHAR(255) NOT NULL,
   peso int NOT NULL,
