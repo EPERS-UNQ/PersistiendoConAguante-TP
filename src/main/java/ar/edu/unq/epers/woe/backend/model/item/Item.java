5f332865d2484f5f96bc9a3501c341de182d30f3
mailinsz <mailinsonez@gmail.com>
2018-04-20 21:05:22 -0300

    Agrego los atributos de la clase Item, faltan implementaciones

--- src/main/java/ar/edu/unq/epers/woe/backend/model/item/Item.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/item/Item.java
@@ -1,5 +1,79 @@
 package ar.edu.unq.epers.woe.backend.model.item;
 
+import java.util.Set;
+
+import ar.edu.unq.epers.woe.backend.model.raza.Clase;
+
+
+
 public class Item {
+	
+	private String nombre;
+	private String ubicacion;
+	private String tipo;
+	private Set<Clase> clases;
+	//private Set<Requerimiento> requerimientos;
+	private int costoDeCompra;
+	private int costoDeVenta;
+	//private Set<Atributo> atributos; 
+	
+	public Item(String nombre,String ubicacion,String tipo,
+			    Set<Clase> clases, int costoDeCompra, int costoDeVenta) {
+		this.setNombre(nombre);
+		this.setUbicacion(ubicacion);
+		this.setTipo(tipo);
+		this.setClases(clases);
+		this.setCostoDeCompra(costoDeCompra);
+		this.setCostoDeVenta(costoDeVenta);	
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getCostoDeCompra() {
+		return costoDeCompra;
+	}
+
+	public void setCostoDeCompra(int costoDeCompra) {
+		this.costoDeCompra = costoDeCompra;
+	}
+
+	public int getCostoDeVenta() {
+		return costoDeVenta;
+	}
+
+	public void setCostoDeVenta(int costoDeVenta) {
+		this.costoDeVenta = costoDeVenta;
+	}
+
+	public Set<Clase> getClases() {
+		return clases;
+	}
+
+	public void setClases(Set<Clase> clases) {
+		this.clases = clases;
+	}
+	
 
 }
