d744efe8c24900f245567fd110b2152b2c2dd447
Asta1986 <psljp@protonmail.com>
2018-04-20 21:32:06 -0400

    clases para atributos e inventario

---
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/personaje/Armadura.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.epers.woe.backend.model.personaje;
+
+public class Armadura extends Atributo {
+
+    public Armadura(Float valor) {
+        this.setValor(valor);
+    }
+
+}
---
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/personaje/Atributo.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.epers.woe.backend.model.personaje;
+
+public class Atributo {
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    private Float valor;
+
+}
