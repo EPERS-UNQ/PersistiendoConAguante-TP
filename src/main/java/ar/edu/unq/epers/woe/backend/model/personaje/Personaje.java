b3b3baf75e659fb9e8f82d0cc18ed65ec19367cc
Astaroth <psljp@protonmail.com>
2018-04-24 20:08:04 -0300

    upd branch

--- src/main/java/ar/edu/unq/epers/woe/backend/model/personaje/Personaje.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/personaje/Personaje.java
@@ -2,20 +2,29 @@ package ar.edu.unq.epers.woe.backend.model.personaje;
 
 import ar.edu.unq.epers.woe.backend.model.raza.Clase;
 import ar.edu.unq.epers.woe.backend.model.raza.Raza;
+import java.util.Set;
+
 
 /**
  * Un {@link Personaje} existente en el sistema, el mismo tiene un nombre
  * y pertenece a una {@link Raza} en particular.
- * 
+ *
  * @author Charly Backend
  */
 public class Personaje {
-	
-	private String nombre;
+
+	private String nombre; //único
 	private Raza raza;
 	private Clase clase;
-	private int energia;
-	
+	private Integer nivel;
+	private Integer exp;
+	private Float billetera;
+	//private Inventario inventario;
+	//private mochila Set<Item>;
+	private Set<Atributo> atributos;
+	//private lugar Lugar;
+
+
 	public Personaje(Raza raza, String nombre, Clase clase) {
 		this.raza = raza;
 		this.nombre = nombre;
@@ -23,31 +32,13 @@ public class Personaje {
 	}
 
 
-	/**
-	 * @return el nombre de un personaje (todos los bichos tienen
-	 * nombre). Este NO es el nombre de su raza.
-	 */
+	// Getters y Setters
 	public String getNombre() {
 		return this.nombre;
 	}
-	
-	/**
-	 * @return la raza a la que este personaje pertenece.
-	 */
+
 	public Raza getRaza() {
 		return this.raza;
-	}
-	
-	/**
-	 * @return la cantidad de puntos de energia de este personaje en
-	 * particular. Dicha cantidad crecerá (o decrecerá) conforme
-	 * a este personaje participe en combates contra otros bichomones.
-	 */
-	public int getEnergia() {
-		return this.energia;
-	}
-	public void setEnergia(int energia) {
-		this.energia = energia;
 	}
 
 	public Clase getClase() {
@@ -57,5 +48,46 @@ public class Personaje {
 	public void setClase(Clase clase) {
 		this.clase = clase;
 	}
+
+	public Integer getNivel() {
+		return nivel;
+	}
+
+	public Integer getExp() {
+		return exp;
+	}
+
+	public Float getBilletera() {
+		return billetera;
+	}
+
+	public Set<Atributo> getAtributos() {
+		return atributos;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setRaza(Raza raza) {
+		this.raza = raza;
+	}
+
+	public void setNivel(Integer nivel) {
+		this.nivel = nivel;
+	}
+
+	public void setExp(Integer exp) {
+		this.exp = exp;
+	}
+
+	public void setBilletera(Float billetera) {
+		this.billetera = billetera;
+	}
+
+	public void setAtributos(Set<Atributo> atributos) {
+		this.atributos = atributos;
+	}
+	//
 
 }
