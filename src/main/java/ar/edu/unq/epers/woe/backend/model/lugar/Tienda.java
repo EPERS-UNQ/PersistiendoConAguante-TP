75c0d11f8a51d1ef6705bbb313ab4a27ae760530
Velazquez Mariana <mariaanavqz@gmail.com!git config --global user.email mariaanavqz@gmail.com>
2018-04-27 18:05:32 -0300

    agregado arch Lugar

---
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/lugar/Tienda.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.epers.woe.backend.model.lugar;
+
+import java.util.Set;
+
+import ar.edu.unq.epers.woe.backend.model.item.Item;
+
+public class Tienda extends Lugar {
+
+	private Set<Item> items;
+
+	public Tienda(String nombreLugar) {
+		super(nombreLugar);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setItems(Set<Item> listaItems) {
+		items = listaItems;
+		
+	}
+
+	public Set<Item> getItems() {
+		return items;
+	}
+
+}
