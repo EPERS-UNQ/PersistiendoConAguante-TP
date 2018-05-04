78664b4f887313fac43cb22fdefd488044eff9ad
Asta1986 <psljp@protonmail.com>
2018-05-04 18:29:36 -0400

    agregado metodo mover

--- src/main/java/ar/edu/unq/epers/woe/backend/service/lugar/LugarService.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/service/lugar/LugarService.java
@@ -1,4 +1,12 @@
 package ar.edu.unq.epers.woe.backend.service.lugar;
 
+import ar.edu.unq.epers.woe.backend.model.lugar.Lugar;
+import ar.edu.unq.epers.woe.backend.model.personaje.Personaje;
+
 public class LugarService {
+
+    public void mover(Personaje pj, Lugar lugar) {
+        pj.setLugar(lugar);
+    }
+    
 }
