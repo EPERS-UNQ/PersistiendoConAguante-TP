f1e6728fee95c24dec82bbe6cf9781a9fe79850d
Asta1986 <psljp@protonmail.com>
2018-05-18 19:23:56 -0400

    persistencia taberna

--- src/main/java/ar/edu/unq/epers/woe/backend/model/lugar/Taberna.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/lugar/Taberna.java
@@ -1,5 +1,10 @@
 package ar.edu.unq.epers.woe.backend.model.lugar;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("TABERNA")
 public class Taberna extends Lugar {
 
 	public Taberna(String nombreLugar) {
