ee7bb02f6476e3dd8790311f2b404a9e3af8c82c
Astaroth <psljp@protonmail.com>
2018-06-14 13:28:38 -0300

    definición de métodos de Luchador como abstractos para #27

--- src/main/java/ar/edu/unq/epers/woe/backend/model/combate/Luchador.java
+++ src/main/java/ar/edu/unq/epers/woe/backend/model/combate/Luchador.java
@@ -4,8 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.MappedSuperclass;
-
 import ar.edu.unq.epers.woe.backend.model.personaje.Danho;
 import ar.edu.unq.epers.woe.backend.model.personaje.Vida;
 
@@ -16,32 +14,20 @@ public abstract class Luchador {
 	@Id
 	protected String nombre; //único
 	
-	public Vida getVida() {
-		return null;
-	}
+	public abstract Vida getVida();
 
-	public Danho getDanhoTotal() {
-		return null;
-	}
+	public abstract Danho getDanhoTotal();
 
-	public void atacar(Luchador l2) {}
+	public abstract void atacar(Luchador l2);
 
-	public void recibirAtaque(Danho danhoTotal){}
+	public abstract void recibirAtaque(Danho danhoTotal);
 
-	public void setVida(Vida vl1) {}
+	public abstract void setVida(Vida vl1);
 
-	public Danho calcularDanhoRecibido(Danho danho) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public abstract Danho calcularDanhoRecibido(Danho danho);
 
-	public boolean sosPersonaje() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public abstract boolean sosPersonaje();
 
-	public boolean sosMonstruo() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public abstract boolean sosMonstruo();
+
 }
