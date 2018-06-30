1b97385c9a066345a01c3cc56d0216c9f938566f
Velazquez Mariana <mariaanavqz@gmail.com!git config --global user.email mariaanavqz@gmail.com>
2018-06-30 20:48:01 -0300

    agregado arch FeedService

---
+++ src/main/java/ar/edu/unq/epers/woe/backend/service/feed/FeedService.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.epers.woe.backend.service.feed;
+
+import java.util.List;
+
+import ar.edu.unq.epers.woe.backend.model.evento.Evento;
+
+public class FeedService {
+	
+	/*
+	 * Devuelve la lista de eventos que involucren al personaje provisto,
+	 * conteniendo primero los eventos mas recientes
+	 */
+	public List<Evento> feedPersonaje(){
+		
+		return null;
+	}
+
+	
+	/*
+	 * Devuelve los eventos que ocurrieron en el lugar mas todos los eventos de los
+	 * lugares que esten conectados con el mismo, conteniendo primero los eventos
+	 * mas recientes
+	 */
+	public List<Evento> feedLugar(){
+		
+		return null;
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/epers/woe/backend/service/FeedServiceTest.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.epers.woe.backend.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.edu.unq.epers.woe.backend.service.feed.FeedService;
+
+public class FeedServiceTest {
+
+	FeedService feedServ;
+	
+	@Before
+	public void setUp(){
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
